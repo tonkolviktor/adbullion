fe03be7a5fa4148736ded314cea889dcf7fcd888
tonkol <tonkol.viktor@danubiusinfo.hu>
2015-12-19 17:15:21 +0100

    product selector

--- app/order/order-directives.js
+++ app/order/order-directives.js
@@ -2,27 +2,58 @@
 
 angular.module('myApp.order.directives', [])
 
-    .directive('orderForm', [function() {
+    .directive('orderForm', [function () {
         return {
             templateUrl: 'order/partials/order-form.html'
         };
     }])
-    .directive('termsAndConditions', [function() {
+    .directive('termsAndConditions', [function () {
         return {
             templateUrl: 'order/partials/terms-and-conditions.html'
         };
     }])
-    .directive('orderSummary', [function() {
+    .directive('orderSummary', [function () {
 
         return {
             restrict: 'E',
+            scope: true,
             templateUrl: 'order/partials/order-summary.html',
-            controller: function($scope, $element){
+            controller: function ($scope, $element) {
                 $scope.products = [
-                    {name: 'Product 1', price: '50'},
-                    {name: 'Shipping', price: '4.95'}
+                    {id: 1, name: 'Product 1', price: '50'},
+                    {id: 2, name: 'Shipping', price: '4.95'}
                 ];
                 $scope.total = 54.95;
+            }
+        };
+    }])
+    .directive('productSelector', [function () {
+        return {
+            restrict: 'E',
+            scope: true,
+            templateUrl: 'order/partials/product-selector.html',
+            controller: function ($scope, $element) {
+                // TODO from DB
+                var dbProducts = [
+                    {id: 1, product: 'One Bottle Adv', price: 45},
+                    {id: 2, product: 'Two Bottles Adv', price: 75},
+                    {id: 3, product: 'Three Bottles Adv', price: 100}
+                ];
+
+                $scope.products = [];
+                $scope.selectedProduct = {id: 0};
+
+                dbProducts.forEach(function(element) {
+                    if(element.product.indexOf("Two") === 0) {
+                        element.image = "gfx/products/more.png";
+                    } else if(element.product.indexOf("Three") === 0) {
+                        element.image = "gfx/products/many.png";
+                    } else {
+                        element.image = "gfx/products/few.png";
+                    }
+
+                    $scope.products.push(element);
+                });
             }
         };
     }])
