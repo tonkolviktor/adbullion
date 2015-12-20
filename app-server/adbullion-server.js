f817ff1457a40401af4ed72ee9b1ba9667ffb4ae
tonkol <tonkol.viktor@danubiusinfo.hu>
2015-12-20 23:40:52 +0100

    clever cloud

--- app-server/adbullion-server.js
+++ app-server/adbullion-server.js
@@ -5,10 +5,10 @@ var DB_USER = process.env["DB_USER"];
 var DB_PASSWORD = process.env["DB_PASSWORD"];
 
 var args = process.argv.slice(2);
-if(!DB_HOST && !DB_USER && !DB_PASSWORD && args.length < 3) {
+if (!DB_HOST && !DB_USER && !DB_PASSWORD && args.length < 3) {
     console.log('Missing database parameters! Usage: (npm start | node app-server/adbullion-server.js) db_host db_user db_password');
     process.exit(1);
-} else {
+} else if (args.length >= 3) {
     DB_HOST = args[0];
     DB_USER = args[1];
     DB_PASSWORD = args[2];
@@ -25,11 +25,11 @@ console.log('Server running at port 8080...');
 
 /* Methods */
 function getServerConfig() {
-    return function(req, res) {
+    return function (req, res) {
         console.log('Receving request: ' + req.url);
 
         // redirect
-        if(req.url == './') {
+        if (req.url == './') {
             res.writeHead(302, {
                 'Location': 'index.html'
             });
@@ -37,7 +37,7 @@ function getServerConfig() {
         }
 
         // serve static files
-        if(req.url.indexOf('/api/') < 0 ) {
+        if (req.url.indexOf('/api/') < 0) {
             buffet(req, res, function () {
                 buffet.notFound(req, res);
             });
@@ -51,14 +51,14 @@ function getServerConfig() {
 
 
 function serveApiRequests(req, res) {
-    var callback = function(err, result) {
+    var callback = function (err, result) {
         var responseCode = 200;
-        if(err) {
+        if (err) {
             responseCode: 500;
             result = JSON.stringify(err);
         }
         res.writeHead(responseCode, {
-            'Content-Type' : 'x-application/json'
+            'Content-Type': 'x-application/json'
         });
         console.log('json:', result);
         res.end(result);
@@ -67,15 +67,15 @@ function serveApiRequests(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Origin', '*');
     var headers = req.headers['access-control-request-headers'];
-    if(headers) {
+    if (headers) {
         res.setHeader('Access-Control-Allow-Headers', headers);
     }
 
     if (req.method === 'OPTIONS') {
         callback(null, '');
-    } else if(req.method === 'GET') {
+    } else if (req.method === 'GET') {
         listData(req.url, callback);
-    } else if(req.method === 'POST') {
+    } else if (req.method === 'POST') {
         saveNewOrder(req, callback);
     } else {
         callback({error: 'Not supported HTTP method: ' + req.method}, null);
@@ -101,7 +101,7 @@ function saveNewOrder(req, callback) {
 function insertOrderIntoDatabase(postData, callback) {
     var connection = getConnectionToQuizDatabase();
     connection.connect();
-    var query = connection.query('INSERT INTO viktorTonkol_sales SET ?', postData, function(err, result) {
+    var query = connection.query('INSERT INTO viktorTonkol_sales SET ?', postData, function (err, result) {
         if (err) {
             return callback(err, null);
         }
@@ -116,9 +116,9 @@ function listData(url, callback) {
     var connection = getConnectionToGeneralDatabase();
     var json, query = '';
 
-    if(url.indexOf('/products') >= 0) {
+    if (url.indexOf('/products') >= 0) {
         query = 'SELECT * FROM products';
-    } else if(url.indexOf('/countries') >= 0) {
+    } else if (url.indexOf('/countries') >= 0) {
         query = 'SELECT * FROM countries';
     } else {
         var err = {message: 'Unknown url: ' + url};
@@ -126,7 +126,7 @@ function listData(url, callback) {
     }
 
     connection.connect();
-    connection.query(query, function(err, results, fields) {
+    connection.query(query, function (err, results, fields) {
         if (err) {
             return callback(err, null);
         }
@@ -140,10 +140,10 @@ function listData(url, callback) {
 function getConnectionToGeneralDatabase() {
     var mysql = require('mysql');
     var connection = mysql.createConnection({
-        host : DB_HOST,
-        user : DB_USER,
-        password : DB_PASSWORD,
-        database : 'quiz_general'
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: 'quiz_general'
     });
 
     return connection;
@@ -152,10 +152,10 @@ function getConnectionToGeneralDatabase() {
 function getConnectionToQuizDatabase() {
     var mysql = require('mysql');
     var connection = mysql.createConnection({
-        host : DB_HOST,
-        user : DB_USER,
-        password : DB_PASSWORD,
-        database : 'QuizTest'
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: 'QuizTest'
     });
 
     return connection;
