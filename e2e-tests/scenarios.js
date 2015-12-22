03273965594abde7451e947226b3dba17dc1f636
tonkol <tonkol.viktor@danubiusinfo.hu>
2015-12-22 18:24:17 +0100

    add commented lines

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -24,36 +24,36 @@ describe('my app', function () {
         browser.get('index.html');
         expect(browser.getLocationAbsUrl()).toMatch("/order");
     });
-    //
-    //
-    //describe('view1', function () {
-    //
-    //    beforeEach(function () {
-    //        browser.get('index.html#/view1');
-    //    });
-    //
-    //
-    //    it('should render view1 when user navigates to /view1', function () {
-    //        expect(element.all(by.css('[ng-view] p')).first().getText()).
-    //            toMatch(/partial for view 1/);
-    //    });
-    //
-    //});
-    //
-    //
-    //describe('view2', function () {
-    //
-    //    beforeEach(function () {
-    //        browser.get('index.html#/view2');
-    //    });
-    //
-    //
-    //    it('should render view2 when user navigates to /view2', function () {
-    //        expect(element.all(by.css('[ng-view] p')).first().getText()).
-    //            toMatch(/partial for view 2/);
-    //    });
-    //
-    //});
+
+
+    describe('view1', function () {
+
+        beforeEach(function () {
+            browser.get('index.html#/view1');
+        });
+
+
+        it('should render view1 when user navigates to /view1', function () {
+            expect(element.all(by.css('[ng-view] p')).first().getText()).
+                toMatch(/partial for view 1/);
+        });
+
+    });
+
+
+    describe('view2', function () {
+
+        beforeEach(function () {
+            browser.get('index.html#/view2');
+        });
+
+
+        it('should render view2 when user navigates to /view2', function () {
+            expect(element.all(by.css('[ng-view] p')).first().getText()).
+                toMatch(/partial for view 2/);
+        });
+
+    });
 
     describe('order', function () {
 
